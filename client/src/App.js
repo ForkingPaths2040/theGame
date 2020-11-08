8faa0d41c7b43a1a87d1ee93728c852a41348cda
Amber Moroe <ambermoorexc@gmail.com>
2020-11-07 19:00:29 -0600

    npm DatePicker and added property values

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React, {useState} from 'react'
 import './App.css';
+import DatePicker from 'react-date-picker'
 
 function App() {
+  const [selected, setSelected] = useState(null);
   return (
     <div className="App">
-
+      <form>
+        <DatePicker
+          selected={selected}
+          onChange={date => setSelected(date)}
+          isClearable
+          showYearDropdown
+          scrollableMonthYearDropdown
+        />
+      </form>
     </div>
   );
 }
